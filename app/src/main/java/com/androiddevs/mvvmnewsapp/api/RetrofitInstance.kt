95457a729585ab11fdc7c93cd2e4fc48445cebf0
Reza Maulana <rflash95@gmail.com>
2021-05-18 19:33:46 +0700

    first commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/RetrofitInstance.kt
@@ -0,0 +1,26 @@
+package com.androiddevs.mvvmnewsapp.api
+
+import com.androiddevs.mvvmnewsapp.util.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+class RetrofitInstance {
+    companion object{
+        private val retrofit by lazy{
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(retrofit2.converter.gson.GsonConverterFactory.create())
+                .build()
+        }
+
+        val api by lazy{
+            retrofit.create(NewsApi::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY ="e12f31e733a5468f9e0790026282278d"
+        const val BASE_URL ="http://newsapi.org"
+    }
+}
