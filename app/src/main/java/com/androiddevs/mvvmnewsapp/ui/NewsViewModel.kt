01cbf19ea81ebf04c1813e00df38b7c5ae812a23
Reza Maulana <rflash95@gmail.com>
2021-05-21 08:48:32 +0700

    .

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -1,9 +1,14 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.androiddevs.mvvmnewsapp.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.util.Resource
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>>
 }
