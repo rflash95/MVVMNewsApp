39151ee52f7429c80d86014718d1f77e9d6981e3
Reza Maulana <rflash95@gmail.com>
2021-06-01 21:05:50 +0700

    Handling Pagination With Retrofit

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY ="e12f31e733a5468f9e0790026282278d"
         const val BASE_URL ="https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
